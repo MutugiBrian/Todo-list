ac02e30561c8e08800cff2949300cf039d8e3bf1
Samuel Mwamburi <sam.mwamburi@gmail.com>
2019-07-19 10:20:49 +0300

    19/07/19 (2)

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+	public function __construct() {
+        parent::__construct();
+
+		$this->load->helper('url');
+		$this->load->model('queries');
+
+    }
 	/**
 	 * Index Page for this controller.
 	 *
@@ -123,9 +130,10 @@ class Welcome extends CI_Controller {
 	}		
 
 	public function chart(){
+		//$data['title'] = 'Funding Trends';
 		$this->load->model('queries');
 		$data = $this->queries->get_data()->result();
-		$x['data'] = json_encode($data);
-		$this->load->view('charts',$x);
+		$data['data'] = json_encode($data);
+		$this->load->view('charts', $data);
 	  }
 }
--- application/views/charts.php
+++ application/views/charts.php
@@ -1,24 +1,41 @@
 <?php include_once('header.php'); ?>
 
-    <h2>Project Funding Trends</h2>
+    <h4>Project Funding Trends</h4>
 
         <div id="graph"></div>
-
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
         <script src="<?php echo base_url().'assets/js/jquery.min.js'?>"></script>
         <script src="<?php echo base_url().'assets/js/highcharts.js'?>"></script>
         <script src="<?php echo base_url().'assets/js/highcharts-3d.js'?>"></script>
         <script src="<?php echo base_url().'assets/js/highcharts-more.js'?>"></script>
         <script type="text/javascript">
-            jQuery (document).ready(function()
-            {
-                
+        var chart1;
+        jQuery(doument).ready(function()
+        {
+            chart1 = new Highcharts.Chart({
+                chart: {
+                    rednderTo: 'container',
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Project Funding'
+                },
+                xAxis: {
+                    categories: ['Project 1', 'Project 2', 'Project 3']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Funding Cost'
+                    }
+                },
             })
-            //Morris.Bar({
-            //element: 'graph',
-            //data: <?php echo $data;?>,
-            //xkey: 'project_ref',
-            //ykeys: ['duration', 'grant_amount', 'status'],
-            //labels: ['Duration', 'Grant Amount (in $)', 'Status']});
+        })
+            Morris.Bar({
+            element: 'graph',
+            data: <?php echo $data;?>,
+            xkey: 'project_ref',
+            ykeys: ['duration', 'grant_amount', 'status'],
+            labels: ['Duration', 'Grant Amount (in $)', 'Status']})
         </script>
 
     </div>
