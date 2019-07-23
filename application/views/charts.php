d1805828fa2f39d1924dcdf02576a749d4b82d6d
Samuel Mwamburi <sam.mwamburi@gmail.com>
2019-07-23 15:10:13 +0300

    23/07/19

--- application/views/charts.php
+++ application/views/charts.php
@@ -5,13 +5,12 @@
         <script src="<?php echo base_url().'assets/js/highcharts-3d.js'?>"></script>
         <script src="<?php echo base_url().'assets/js/highcharts-more.js'?>"></script>
         
-        <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
-        
-        <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
-        <div class="container-fluid my-5" id="graph">
         <h4>Project Funding Trends</h4>
-        <?php include("path/to/local/fusioncharts.php"); ?>
-        <?php $Chart = new FusionCharts("column2d", "MyFirstChart" , "700", "400", "chart-container", "json", $jsonEncodedData);?>
+        
+        <div class="container-fluid my-5" id="graph">
+        
+        
+        
         </div>
     </div>
     <?php echo anchor('welcome', 'Go Back', ['class'=>'btn btn-danger']);?>
